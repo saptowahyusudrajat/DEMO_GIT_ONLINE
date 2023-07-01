a5cbab159f2f5b8a250225398d1b537ee6474038
saptowahyusudrajat <mtvgsapto@gmail.com>
2023-07-01 14:19:34 +0700

    perubahan kedua menambahkan input untuk alas dan tinggi

--- formula_luas_bangun.py
+++ formula_luas_bangun.py
@@ -1,10 +1,8 @@
 def luas_segitiga():
-    a  = 12
-    t =   6
+    a  = int(input("Masukkan alas segitiga: "))
+    t =  int(input("Masukkan tinggi segitiga: "))
 
     luas =  0.5*a*t
     print("luas segitiga adalah: ", luas)
 
 luas_segitiga()
-
-
