a6f8602f1da7e1f57b1c74b9a576b4e6f23acef3
saptowahyusudrajat <mtvgsapto@gmail.com>
2023-07-01 14:40:30 +0700

    Ditambahkab fitur untuk menghitung luas persegi panjang

--- formula_luas_bangun.py
+++ formula_luas_bangun.py
@@ -6,3 +6,13 @@ def luas_segitiga():
     print("luas segitiga adalah: ", luas)
 
 luas_segitiga()
+
+
+def luas_segi_panjang():
+    p = int(input("Masukkan panjang persegi panjang: "))
+    l = int(input("Masukkan lebar persegi panjang: "))
+
+    luas = p*l
+    print("luas persegi panjang adalah: ", luas)
+
+luas_segi_panjang()
